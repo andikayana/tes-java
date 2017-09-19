43895c37c06c312b243bd5328e56b7602ee1bbfc
andikayana <andikayana99@gmail.com>
2017-09-19 14:24:49 +0800

    Tes Java2

--- src/penjumlahan/Penjumlahan.java
+++ src/penjumlahan/Penjumlahan.java
@@ -3,7 +3,12 @@ package penjumlahan;
 public class Penjumlahan {
 
     public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
         
+        c = a+b;
+        System.out.println(c);
     }
     
 }
